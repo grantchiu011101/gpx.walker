f3839eb542fad804ed47db5617f5caa69763d119
Grant Fong <grantchiu011101@gmail.com>
2016-08-01 09:32:49 +0800

    generate gpx.stop and gpx.walker file

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var fs = require('fs') ;
 
 
 var url = "https://maps.googleapis.com/maps/api/geocode/json?&address=" + encodeURIComponent(process.argv[2]) ;
+console.log ( process.argv) ;
 console.log ( "%s %s", moment().format(), url) ;
+
 
 
 request.get({url: url, json: true}, function(e, r, data) {
@@ -16,41 +18,52 @@ request.get({url: url, json: true}, function(e, r, data) {
 	var location = _.first(data.results).geometry.location ;
 	var initial = {lat:location.lat, lon: location.lng} ;
 	console.log ( "%s %s", moment().format(), _.first(data.results).formatted_address ) ;
+	
 
-	var wpt = _.chain(_.range(100)).map(function(val) {
-		
-		var dist = 200;
-		var angle = _.chain(_.range(12)).map(function(val){ return val * 30}).value() ;
-		
-		var bearing = _.sample (angle)
-		
+	_.each ( _.range(2), function(value) {
+		var stop = (value === 1)  ;
 
-		var result = geolib.computeDestinationPoint(this.initial, dist, bearing);
-		this.initial = result ;
-		// console.log ("%s %s %s", moment().format(), result.latitude, result.longitude ) ;
-		return {
+		var step = 300 ;
+		var wpt = _.chain(_.range(step)).map(function(val) {
+			
+			var dist = 150 ;
+			var angle = _.chain(_.range(12)).map(function(val){ return val * 30}).value() ;
+			
+
+			// console.log ( (val % angle.length)) ;
+			var bearing = (this.stop)?angle[(val % angle.length)]:_.sample(angle) ;
+			// console.log ( bearing) ;
+			
+
+			var result = geolib.computeDestinationPoint(this.initial, dist, bearing);
+			this.initial = result ;
+			return {
+				"@": {
+					lat: result.latitude ,
+					lon: result.longitude
+				},
+				"time":moment().add(val, 'm').format("YYYY-MM-DDTHH:mm:ss")
+			}
+
+		}, {config: config, initial: initial, stop: stop}).value() ;
+
+		var data = {
 			"@": {
-				lat: result.latitude ,
-				lon: result.longitude
+				"xmlns": "http://www.topografix.com/GPX/1/1",
+				"version": "1.1",
+				"creator": "walker"
 			},
-			"time": moment().add(val, 'm').format("YYYY-MM-DDTHH:mm:ss")
-		}
+			"wpt": wpt
+		};
 
-	}, {config: config, initial: initial}).value() ;
+		// console.log(js2xmlparser("gpx", data));
 
-	var data = {
-		"@": {
-			"xmlns": "http://www.topografix.com/GPX/1/1",
-			"version": "1.1",
-			"creator": "walker"
-		},
-		"wpt": wpt
-	};
-
-	// console.log(js2xmlparser("gpx", data));
-
-	var gpxFile = fs.createWriteStream("GPXWalker.gpx") ;
-	gpxFile.write(js2xmlparser("gpx", data)) ;
+		var gpxFile = fs.createWriteStream(((stop)?"gpx.stop.gpx":"gpx.walker.gpx")) ;
+		gpxFile.write(js2xmlparser("gpx", data)) ;
+		console.log ( '%s %s' , moment().format(),((stop)?"gpx.stop.gpx":"gpx.walker.gpx")) ;
+		
+	}) ;
 	console.log ( '%s Generate GPX complete %s', moment().format(), process.argv[2]) ;
+	
 
 }) ;
